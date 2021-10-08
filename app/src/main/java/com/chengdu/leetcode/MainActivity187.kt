04cfa4858951a44b229c53ac14a7b0ff1c555acd
jieyu <yujieworking@gmail.com>
2021-10-08 14:36:07 +0800

    20211008

--- app/src/main/java/com/chengdu/leetcode/MainActivity187.kt
+++ app/src/main/java/com/chengdu/leetcode/MainActivity187.kt
@@ -3,7 +3,7 @@ package com.chengdu.leetcode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity187 : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
