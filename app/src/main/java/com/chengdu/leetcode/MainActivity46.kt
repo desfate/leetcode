64095455bf5a6459b5d95b92da05365bfd01afdf
yu jie <yujieworking@gmail.com>
2022-08-15 13:25:50 +0800

    commit 46 全排列 暴力递归

--- app/src/main/java/com/chengdu/leetcode/MainActivity46.kt
+++ app/src/main/java/com/chengdu/leetcode/MainActivity46.kt
@@ -10,10 +10,36 @@ class MainActivity46 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
-    val result = listOf<List<Int>>()
 
+    val result = arrayListOf<List<Int>>()
+
+    fun permute(nums: IntArray): List<List<Int>> {
+        permute(ArrayList<Int>(nums.asList()), "");
+        return result
+    }
+
+
+    fun permute(nums: ArrayList<Int>, head: String) {
+        if (nums.isEmpty()) {
+            result.add(head.split("_").map { it.toInt() })
+        }
+        // 等于对子问题求解
+        nums.forEach {
+            val list = arrayListOf<Int>()
+            list.addAll(nums)
+            list.remove(it)
+            permute(
+                list, if (head.isEmpty()) {
+                    it.toString()
+                } else {
+                    head + "_" + it
+                }
+            )
+        }
+    }
 
 }
 
